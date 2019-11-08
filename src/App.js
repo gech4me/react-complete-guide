c17999f406e6ea53c79a4d4b868bc654fda5a726
Getachew Mulat <gech4me@gmail.com>
2019-11-08 14:41:44 +0300

    function app changed to class to include state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, Started</h1>
-      <p>This is really working</p>
-      <Person name="Getachew" age="27"/>
-      <Person name="Alemu" age="10">My hobbies: Racing </Person>
-      <Person name="John" age="28"/>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+        <div className="App">
+          <h1>Hi, Started</h1>
+          <p>This is really working</p>
+          <Person name="Getachew" age="27"/>
+          <Person name="Alemu" age="10">My hobbies: Racing </Person>
+          <Person name="John" age="28"/>
+        </div>
+    );
+  }
 }
 
 export default App;
