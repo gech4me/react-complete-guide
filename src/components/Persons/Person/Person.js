e783e95d1fc454d0f41bc02dcb2d88e82803cd30
Getachew Mulat <gech4me@gmail.com>
2019-11-12 12:12:38 +0300

    using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,7 +7,7 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-     <Aux>
+     <React.Fragment>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -17,7 +17,7 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-     </Aux>
+     </React.Fragment>
     );
   }
 }
