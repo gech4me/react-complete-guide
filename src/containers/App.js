b28b933cda748c27c0a325025a309e198017f779
Getachew Mulat <gech4me@gmail.com>
2019-11-12 21:39:00 +0300

    another form HOCs

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from "../hoc/WithClass";
+import Aux from "../hoc/Aux";
+import withClass from "../hoc/withClass";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -89,7 +90,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -106,9 +107,9 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App,classes.App);
