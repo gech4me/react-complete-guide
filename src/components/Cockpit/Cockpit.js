87184fc9dba7f66cfa7f559c8b4428a913fbdca5
Getachew Mulat <gech4me@gmail.com>
2019-11-12 11:02:53 +0300

    cleanup work with useEffect-ex

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,11 +6,12 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('Cockpit.js useEffect');
 
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             alert('Saved data to cloud!');
         },1000);
 
         return () => {
+            clearTimeout(timer);
             console.log('Cockpit.js cleanup work in useEffect');
         }
     }, []);
